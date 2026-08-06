1e5a9ea51aecf647d3a52827594797750e665361
fajalkazi8522-afk <fajalkazi8522@gmail.com>
2026-08-06 18:23:33 +0530

    Implement savePatient function for patient data
    Added a function to save patient data to local storage.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ function showMessage(message){
 }
 
 console.log("Varahi Laboratory Management System Loaded Successfully");
+function savePatient(){
+
+let patient={
+
+name:document.getElementById("name").value,
+
+age:document.getElementById("age").value,
+
+gender:document.getElementById("gender").value,
+
+mobile:document.getElementById("mobile").value,
+
+address:document.getElementById("address").value
+
+};
+
+localStorage.setItem("patient",JSON.stringify(patient));
+
+alert("Patient Saved Successfully");
+
+}
